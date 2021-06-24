86ab2c60db31d8ca2c28770977304677ce0db1d4
ammilunovic <aleksandarmilunovic10@gmail.com>
2021-06-24 13:58:46 +0200

    Upgraded 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { AuthorComponent } from './components/core/author/author.component';
+import { AboutComponent } from './components/core/about/about.component';
+import { HomeComponent } from './components/core/home/home.component';
+import { KreditComponent } from './components/kredit/kredit.component';
+import { TipRacunaComponent } from './components/tip-racuna/tip-racuna.component';
+import { KlijentComponent } from './components/klijent/klijent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthorComponent,
+    AboutComponent,
+    HomeComponent,
+    KreditComponent,
+    TipRacunaComponent,
+    KlijentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatGridListModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
