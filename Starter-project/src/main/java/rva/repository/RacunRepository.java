54dbbbbbc9da40990fb4679b189f109cc964d92d
ammilunovic <Aca>
2021-06-26 00:57:25 +0200

    Krajnja izmena

--- Starter-project/src/main/java/rva/repository/RacunRepository.java
+++ Starter-project/src/main/java/rva/repository/RacunRepository.java
@@ -5,9 +5,11 @@ import java.util.Collection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import rva.jpa.Racun;
+import rva.jpa.TipRacuna;
 
 public interface RacunRepository extends JpaRepository<Racun, Integer>{
 	
 	Collection<Racun> findByOznakaContainingIgnoreCase(String oznaka);
+	Collection<Racun> findByTipRacuna(TipRacuna tp);
 
 }
